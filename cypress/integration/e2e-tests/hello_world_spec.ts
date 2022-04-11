4711dbcc2cb49b35bd7ccd2447e1495e9e6571ca
PipsaKorkiakoski <43844178+PipsaKorkiakoski@users.noreply.github.com>
2022-04-11 14:06:13 +0300

    cross-origin debug

--- cypress/integration/e2e-tests/hello_world_spec.ts
+++ cypress/integration/e2e-tests/hello_world_spec.ts
@@ -4,7 +4,7 @@ describe("First e2e tests", () => {
   before(() => {
     console.log(Cypress.config().baseUrl);
     Cypress.config().baseUrl = "https://demo2022pipsa.auth.eu-west-1.amazoncognito.com/login?client_id=2h8a2cqb3l46gu1bviglk89n4b&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://gorest.co.in";
-
+    cy.task("log", Cypress.config().baseUrl)
     cy.visit("");
     cy.url().should("include", "/login");
     cy.task<User>("getUserPassword", alma_user).then((user: User) => {
