d10fa791308ea5b09e05f57febe530dc0326ebcb
PipsaKorkiakoski <43844178+PipsaKorkiakoski@users.noreply.github.com>
2022-04-21 11:23:16 +0300

    Update user.ts

--- cypress/support/user.ts
+++ cypress/support/user.ts
@@ -10,63 +10,16 @@ export interface User {
   secretId: string;
 }
 
-export const alma_admin: User = {
-  userId: "",
-  name: "Admin",
-  user_name: "admin@almamedia.local",
-  role: "",
-  permissions: [],
-  email: "admin@almamedia.local",
-  password: "",
-  secretName: "<add_secretName_here>",
-  secretId: "<add_secret's_arn_here>"
-};
-
-export const alma_user: User = {
+export const test_user: User = {
   userId: "",
   name: "User",
-  user_name: "user@example.local",
+  user_name: "user@example.com",
   role: "",
   permissions: [],
-  email: "user@example.local",
+  email: "user@example.com",
   password: "",
-  secretName: "dev/test/alma_user",
+  secretName: "dev/test/test_user",
   secretId:
-    "arn:aws:secretsmanager:eu-west-1:590130834747:secret:dev/test/alma_user-xLwlTQ"
+    "arn:aws:secretsmanager:eu-west-1:xxxQ"
 };
 
-export const alma_apitest: User = {
-  userId: "",
-  name: "Apitest",
-  user_name: "apitest@almamedia.local",
-  role: "",
-  permissions: [],
-  email: "apitest@almamedia.local",
-  password: "",
-  secretName: "<add_secretName_here>",
-  secretId: "<add_secret's_arn_here>"
-};
-
-export const alma_e2etest: User = {
-  userId: "",
-  name: "E2Etest",
-  user_name: "e2etest@almamedia.local",
-  role: "",
-  permissions: [],
-  email: "e2etest@almamedia.local",
-  password: "",
-  secretName: "<add_secretName_here>",
-  secretId: "<add_secret's_arn_here>"
-};
-
-export const alma_apitest_admin: User = {
-  userId: "",
-  name: "Apitest Admin",
-  user_name: "apitestadmin@almamedia.local",
-  role: "",
-  permissions: [],
-  email: "apitestadmin@almamedia.local",
-  password: "",
-  secretName: "<add_secretName_here>",
-  secretId: "<add_secret's_arn_here>"
-};
