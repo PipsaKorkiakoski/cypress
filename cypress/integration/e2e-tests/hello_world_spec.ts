f0f09090ec1bb9ece712a9437961d85f7a70848d
PipsaKorkiakoski <43844178+PipsaKorkiakoski@users.noreply.github.com>
2022-04-21 11:22:02 +0300

    Update hello_world_spec.ts

--- cypress/integration/e2e-tests/hello_world_spec.ts
+++ cypress/integration/e2e-tests/hello_world_spec.ts
@@ -1,13 +1,13 @@
-import { User, alma_user } from "../../support/user";
+import { User, test_user } from "../../support/user";
 
 describe("First e2e tests", () => {
   before(() => {
     console.log(Cypress.config().baseUrl);
-    Cypress.config().baseUrl = "https://demo2022pipsa.auth.eu-west-1.amazoncognito.com/login?client_id=2h8a2cqb3l46gu1bviglk89n4b&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://gorest.co.in";
+    Cypress.config().baseUrl = "https://demo2022xxxxxredirect_uri=https://gorest.co.in";
     cy.task("log", Cypress.config().baseUrl)
     cy.visit("");
     cy.url().should("include", "/login");
-    cy.task<User>("getUserPassword", alma_user).then((user: User) => {
+    cy.task<User>("getUserPassword", test_user).then((user: User) => {
       cy.login(user);
     });
   });
